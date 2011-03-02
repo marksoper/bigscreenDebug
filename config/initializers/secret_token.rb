1005c6f45372dd5a77667540f489a3f99167d481
mark <mark@ip-10-112-59-206.ec2.internal>
2011-03-02 19:36:39 +0000

    initial code commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsBigscreenMe::Application.config.secret_token = '973ad84ba6652b5540e78dd33771e2e529e102c11d5be6fc66db4e37bd172deb01ad2e3d095d3a3549a8da71964b517cf4e399b288e5fd5cfd1f8d05ca51a7dd'
